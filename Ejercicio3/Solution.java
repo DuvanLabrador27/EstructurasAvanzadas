a52817c7919d94d80e30c6ef58b78d27c7230bc6
Duvanlabrador27 <carlosduvanlh@ufps.edu.co>
2022-12-06 18:52:09 -0500

    Class TreeNode

--- Ejercicio3/Solution.java
+++ Ejercicio3/Solution.java
@@ -1,16 +1,35 @@
-public class Solution {
-    public boolean isBST(TreeNode root,long low,long high)
-    {
-        if (root == null) return true;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-        if (root.val <= low || root.val >= high) return false; 
-
-       
-        return isBST(root.left,low,root.val) && isBST(root.right,root.val,high);
+    TreeNode() {
     }
-    public boolean isValidBST(TreeNode root) 
-    {
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+
+    public boolean isBST(TreeNode root, long low, long high) {
+        if (root == null)
+            return true;
+
+        if (root.val <= low || root.val >= high)
+            return false;
+
+        return isBST(root.left, low, root.val) && isBST(root.right, root.val, high);
+    }
+
+    public boolean isValidBST(TreeNode root) {
         return isBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
-        
     }
 }
