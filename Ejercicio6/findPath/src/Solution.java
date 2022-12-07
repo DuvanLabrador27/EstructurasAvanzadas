51a1cbd499830e5b44b32d8eaf84f5881de8cdbc
saidloop <saidfernandolgon@ufps.edu.co>
2022-12-06 22:57:57 -0500

    "pruebas del ejercicio 6"

--- Ejercicio6/findPath/src/Solution.java
+++ Ejercicio6/findPath/src/Solution.java
@@ -1,6 +1,7 @@
-import java.util.ArrayList;
-import java.util.List;
-
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * * * *   Clase del metodo de conteo de Numeros Distintos en una lista  * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+import java.util.*;
 public class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination){
 
